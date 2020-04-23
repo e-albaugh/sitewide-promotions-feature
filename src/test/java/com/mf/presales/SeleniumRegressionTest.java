dd7a1d947643aa08759482323650608a91bf3fba
Radislav Berkovich <radislav.berkovich@microfocus.com>
2020-04-23 21:58:47 +0300

    initial

---
+++ src/test/java/com/mf/presales/SeleniumRegressionTest.java
@@ -0,0 +1,82 @@
+package com.mf.presales;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class SeleniumRegressionTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public SeleniumRegressionTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(SeleniumRegressionTest.class);
+    }
+
+
+    public void testUserProfCheckout() {
+        assertTrue(true);
+    }
+
+    public void testUserProfUpdates() {
+        assertTrue(true);
+    }
+
+    public void testUserProfLogInOutt() {
+        assertTrue(true);
+    }
+
+    public void testSpeakersOrder() {
+        assertTrue(true);
+    }
+
+    public void testSpeakersRemoveFromCartt() {
+        assertTrue(true);
+    }
+
+    public void testSpeakersInventory() {
+        assertTrue(true);
+    }
+
+    public void testSpeakersNav() {
+        assertTrue(true);
+    }
+
+    public void testTabletsOrder() {
+        assertTrue(true);
+    }
+
+    public void testTabletsRemoveFromCart() {
+        assertTrue(true);
+    }
+
+    public void testTabletsFilterr() {
+        assertTrue(true);
+    }
+
+    public void testSpeakersFilter() {
+        assertTrue(true);
+    }
+
+    public void testTabletsInventory() {
+        assertTrue(true);
+    }
+
+    public void testTabletsNav() {
+        assertTrue(true);
+    }
+
+
+
+}
